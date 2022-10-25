841b76588802d296e8a6424210da2bc6716a74e9
LenkaGolovach <karamba290@gmail.com>
2022-10-25 09:54:07 +0300

    Я сделал индивидуальное!!!

--- ind.py
+++ ind.py
@@ -2,21 +2,32 @@
 # -*- coding: utf-8 -*-
 
 import time
+import collections
 
 
-def br_check(s):
-    start = time.perf_counter()
-    m = 0
-    for c in s:
-        if c == '(':
-            m += 1
-        elif c == ')':
-            m -= 1
-            if m < 0:
+def br_check(list_1):
+    if len(list_1) == 0:
+        return True
+    else:
+        list_1 = list(map(str, list_1))
+        f = list_1[0]
+        a = list_1[-1]
+        if f == '(':
+            if a == ')':
+                list_1.pop(0)
+                list_1.pop(-1)
+                return br_check(list_1)
+            else:
                 return False
-
-    print(f"Время выполнения функции = {time.perf_counter() - start}")
-    return m == 0
+        elif f == '[':
+            if a == ']':
+                list_1.pop(0)
+                list_1.pop(-1)
+                return br_check(list_1)
+            else:
+                return False
+        else:
+            return False
 
 
 if __name__ == '__main__':
